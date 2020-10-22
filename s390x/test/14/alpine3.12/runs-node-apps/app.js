3844c02005f2022da536832cadecaca7370f5010
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-22 06:50:17 -0400

    Create app.js

---
+++ s390x/test/14/alpine3.12/runs-node-apps/app.js
@@ -0,0 +1,4 @@
+const c = require('ansi-colors');
+
+console.log(c.green('Success! This Node.js app is running on the quay.io/ibmz/node image!'));
+console.log(c.green('Test Complete. Terminating...'));
